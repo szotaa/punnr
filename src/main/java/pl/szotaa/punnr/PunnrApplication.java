956de00a9b1d2952fc99d9c138449717d5e48a9a
szotaa <shoothas@gmail.com>
2018-04-07 12:44:16 +0200

    initial spring boot application

---
+++ src/main/java/pl/szotaa/punnr/PunnrApplication.java
@@ -0,0 +1,12 @@
+package pl.szotaa.punnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunnrApplication.class, args);
+	}
+}
