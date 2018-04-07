e6a76dd78264af009da74e5c1f17a16630d675a6
szotaa <shoothas@gmail.com>
2018-04-07 14:23:19 +0200

    initial frontend architecture

--- src/main/java/pl/szotaa/punnr/config/MvcConfig.java
+++ src/main/java/pl/szotaa/punnr/config/MvcConfig.java
@@ -2,10 +2,18 @@ package pl.szotaa.punnr.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry
+                .addViewController("/").setViewName("home");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/pl/szotaa/punnr/config/SecurityConfig.java
+++ src/main/java/pl/szotaa/punnr/config/SecurityConfig.java
@@ -9,6 +9,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/webjars/**").permitAll();
+        http
+                .authorizeRequests().antMatchers("/webjars/**", "/").permitAll()
+                .anyRequest().authenticated();
     }
 }
