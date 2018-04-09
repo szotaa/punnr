7f7d090dbd3ffdce0d400cf3f5b59556f28c1d76
szotaa <shoothas@gmail.com>
2018-04-09 11:24:22 +0200

    initial socket configuration

--- src/main/java/pl/szotaa/punnr/config/SecurityConfig.java
+++ src/main/java/pl/szotaa/punnr/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().antMatchers("/webjars/**", "/", "/user/**").permitAll()
+                .authorizeRequests().antMatchers("/webjars/**", "/", "/user/**", "/game/**").permitAll()
                 .anyRequest().authenticated();
 
         http
