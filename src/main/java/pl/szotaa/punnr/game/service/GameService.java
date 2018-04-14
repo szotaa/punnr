6ec6cc7c95f25c0dd55a9b15e04685fe7cb3abda
szotaa <shoothas@gmail.com>
2018-04-14 16:48:10 +0200

    answers are now stored in external file

--- src/main/java/pl/szotaa/punnr/game/service/GameService.java
+++ src/main/java/pl/szotaa/punnr/game/service/GameService.java
@@ -1,6 +1,9 @@
 package pl.szotaa.punnr.game.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.szotaa.punnr.game.domain.GameRoom;
 import pl.szotaa.punnr.game.holder.GameRoomHolder;
@@ -8,8 +11,13 @@ import pl.szotaa.punnr.game.message.ChatMessage;
 import pl.szotaa.punnr.game.message.Event;
 import pl.szotaa.punnr.game.messenger.Messenger;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadLocalRandom;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GameService {
@@ -28,11 +36,31 @@ public class GameService {
     public void startNewRound(String gameId){
         GameRoom gameRoom = gameRoomHolder.getById(gameId);
         gameRoom.setDrawing(new ConcurrentLinkedQueue<>());
-        gameRoom.setCurrentDrawingTitle("exampleTitle");
+        gameRoom.setCurrentDrawingTitle(getAnotherAnswer(gameRoom.getCurrentDrawingTitle()));
+        log.info(gameRoom.getCurrentDrawingTitle());
         messenger.sendToAll(gameId, new Event(Event.EventType.ROUND_STARTED, null, null));
     }
 
     private boolean isAnswerCorrect(String gameId, String answer){
         return gameRoomHolder.getById(gameId).getCurrentDrawingTitle().compareToIgnoreCase(answer) == 0;
+    }
+
+    private String getAnotherAnswer(String currentAnswer){ //TODO: clean this up
+        ObjectMapper objectMapper = new ObjectMapper();
+        String result = null;
+        try {
+            InputStream answerStream = this.getClass().getResourceAsStream("/answers.json");
+            JsonNode node = objectMapper.readTree(answerStream).get("answers");
+            List<String> answers = new ArrayList<>();
+            node.forEach(answer -> answers.add(answer.asText()));
+            if(currentAnswer != null){
+                answers.remove(currentAnswer);
+            }
+            result = answers.get(ThreadLocalRandom.current().nextInt(0, answers.size()));
+
+        } catch (Exception e){
+
+        }
+        return result;
     }
 }
