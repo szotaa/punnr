717071cbaf8b0cd824d57b4efb495a0287496cc2
szotaa <shoothas@gmail.com>
2018-04-23 11:17:11 +0200

    cleanup tests

--- src/test/java/pl/szotaa/punnr/game/service/GameRoomServiceTest.java
+++ src/test/java/pl/szotaa/punnr/game/service/GameRoomServiceTest.java
@@ -3,12 +3,14 @@ package pl.szotaa.punnr.game.service;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.szotaa.UnitTest;
 import pl.szotaa.punnr.game.domain.GameRoom;
 import pl.szotaa.punnr.game.holder.GameRoomHolder;
+import pl.szotaa.punnr.game.message.Event;
 import pl.szotaa.punnr.game.messenger.Messenger;
 
 import java.util.Map;
@@ -33,6 +35,8 @@ public class GameRoomServiceTest {
     @Mock
     private Messenger messenger;
 
+    private ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
+
     @Test
     public void create_roomGetsCreated(){
         //given
@@ -45,6 +49,35 @@ public class GameRoomServiceTest {
         //then
         verify(gameRoomHolder, times(1)).getGameRooms();
         assertEquals(1, gameRooms.size());
+    }
+
+    @Test
+    public void join_newPlayer_playerJoinedEventGetsSent() {
+        //given
+        GameRoom gameRoom = new GameRoom();
+        when(gameRoomHolder.getById(anyString())).thenReturn(gameRoom);
+
+        //when
+        gameRoomService.join("gameId", "username");
+
+        //then
+        verify(messenger, times(1)).sendToAll(anyString(), eventArgumentCaptor.capture());
+        assertEquals(Event.EventType.PLAYER_JOINED, eventArgumentCaptor.getValue().getEventType());
+    }
+
+    @Test
+    public void join_newPlayer_playerInitialScoreIsSet() {
+        //given
+        GameRoom gameRoom = new GameRoom();
+        when(gameRoomHolder.getById(anyString())).thenReturn(gameRoom);
+
+        //when
+        gameRoomService.join("gameId", "username");
+
+        //then
+        assertEquals(1, gameRoomHolder.getById("gameId").getPlayers().size());
+        assertEquals(1, gameRoomHolder.getById("gameId").getScoreboard().size());
+        assertEquals(0L, (long) gameRoomHolder.getById("gameId").getScoreboard().get("username"));
     }
 
     @Test
